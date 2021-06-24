2ad820f727d46f78443307093c1376df18819d79
PriyanshX1902 <priyansh.bhatnagar@rediff.com>
2021-06-24 22:35:43 +0530

    Setup VideoPlayer Component

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers/index';
+import { ContextProvider } from './SocketContext';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store = {store}>
+    <ContextProvider>
       <App />
+    </ContextProvider>
   </Provider>,
   document.getElementById('root')
 );
