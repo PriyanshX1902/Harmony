bb83c8fa8606ba7ee59124e1dc85df33b24dd387
PriyanshX1902 <priyansh.bhatnagar@rediff.com>
2021-06-24 22:13:28 +0530

    Added basic Socket.io functionalities in frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import {AppBar, Typography } from '@material-ui/core';
+import VideoPlayer from './components/VideoPlayer';
+import Notifications from './components/Notifications';
+import Options from './components/Options';
 
 function App() {
   return (
     <div className="App">
-      Hello!
+      <AppBar position = "static" color = "inherit">
+        <Typography variant = "h2" align = "center">Video Chat</Typography>
+      </AppBar>
+      <VideoPlayer/>
+      <Options>
+        <Notifications/>
+      </Options>
     </div>
   );
 }
