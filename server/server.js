ed41d4b78f9437bbbbdfe2b66919dabe3ebedf8e
PriyanshX1902 <priyansh.bhatnagar@rediff.com>
2021-06-24 21:08:30 +0530

    Added Basic Socket.io functionalities in backend

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ const app = express();
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import route from './routes/sample.js';
+import {createServer} from 'http';
 const port = process.env.port || 5000;
-
+const server = createServer(app);
 app.use(
     cors({
         origin: 'http://localhost:3000/',
@@ -17,4 +18,4 @@ app.get('/', (req, res)=>{
 app.use('/sample', route);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.listen(port, console.log('Listening to port 5000!'));
+server.listen(port, console.log('Listening to port 5000!'));
